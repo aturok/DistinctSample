a5ef195e1e7892329d940bb62fb0a560eeb38ab5
Alexander Turok <chaot8@gmail.com>
2013-03-19 19:14:14 +0400

    First

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistinctSample
+{
+    class MovieActor
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string CharacterName { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} \"{1}\" {2}", FirstName, CharacterName, LastName);
+        }
+
+        public static IEnumerable<MovieActor> CreateSome()
+        {
+            return new List<MovieActor>()
+            {
+                new MovieActor() { FirstName = "Brad", LastName = "Pitt", CharacterName = "Rusty"},
+                new MovieActor() { FirstName = "Andy", LastName = "Garcia", CharacterName = "Terry"},
+                new MovieActor() { FirstName = "George", LastName = "Clooney", CharacterName = "Dany"},
+                new MovieActor() { FirstName = "Jullia", LastName = "Roberts", CharacterName = "Tess"}
+            };
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actors = MovieActor.CreateSome();
+            Console.WriteLine(String.Format("{0} total players.", actors.Count()));
+            foreach (var actor in actors)
+            {
+                Console.WriteLine(actor);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("DistinctSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DistinctSample")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b243512f-5bdb-4428-9c19-758b7b14b12e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
